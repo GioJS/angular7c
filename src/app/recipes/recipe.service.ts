6b00a6fa2c4e8acab4a99324f7300e9b29decb3a
giordano cristini <giordano.cristini@exprivia.com>
2019-02-14 09:52:37 +0100

    ++++++++++++++++++++++++++++++++

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,22 +3,12 @@ import {Injectable} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
 import {Subject} from 'rxjs';
+import {DataStorageService} from '../shared/data-storage.service';
 
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [new Recipe(
-    'A test recipe',
-    'this is a simply test',
-    'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco,c_fill,g_auto,w_960/' +
-    'https://storage.googleapis.com/gen-atmedia/3/2018/06/d83512a0fe4e2d338f89ccde0c5de227646921cf.jpeg',
-    [new Ingredient('Meat', 1), new Ingredient('Bread', 2)]),
-    new Recipe(
-      'A test recipe 2',
-      'this is a simply test',
-      'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco,c_fill,g_auto,w_960/' +
-      'https://storage.googleapis.com/gen-atmedia/3/2018/06/d83512a0fe4e2d338f89ccde0c5de227646921cf.jpeg',
-      [new Ingredient('Salad', 5)])];
+  private recipes: Recipe[] = [];
 
   getRecipes() {
     return this.recipes.slice();
